f50d1428d271f238e0d270b305a276ec0b3e4ed1
Krikrifreeze <krikrifreeze@gmail.com>
2019-09-06 18:00:40 -0700

    I can't find an online example

--- tests/test_multipleLinearRegression.py
+++ tests/test_multipleLinearRegression.py
@@ -11,12 +11,14 @@ from MultipleLinearRegression import MultipleLinearRegression
 
 class TestMultipleLinearRegression(unittest.TestCase):
 
-    def test_deliveryTimesData(self):
-        # http://sphweb.bumc.bu.edu/otlt/MPH-Modules/BS/BS704_Multivariable/BS704_Multivariable7.html
-        #the referenced data: https://biolincc.nhlbi.nih.gov/media/studies/framoffspring/Forms/Offspring%20Cohort%20Exam%207.pdf?link_time=2019-09-06_20:49:07.555681
-        print("TODO")
+    def test_example(self):
+        #TODO: find an online example to try this on!! >:(
+        inputXmatrix = np.ndarray((3,3)) #add values based on the online example to this
+        inputYmatrix = np.nd_grid((3,1)) #add values based on the online example to this
 
-
+        self.assertAlmostEqual(MultipleLinearRegression(inputXmatrix,inputYmatrix)[0],-1,5) #replace -1 with the actual coefficient from the example
+        self.assertAlmostEqual(MultipleLinearRegression(inputXmatrix,inputYmatrix)[1],-1,5) #replace -1 with the actual coefficient from the example
+        self.assertAlmostEqual(MultipleLinearRegression(inputXmatrix,inputYmatrix)[2],-1,5) #replace -1 with the actual coefficient from the example
 
 
 if __name__ == "__main__":
